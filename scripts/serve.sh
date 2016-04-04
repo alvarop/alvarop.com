51e640a075830eb5171b538c9854dee445db611f
Alvaro Prieto <apg88zx@gmail.com>
2016-04-03 21:24:32 -0700

    Only rebuild the needed files when doing jekyll serve

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jekyll serve --source /vagrant --force_polling --watch --host 0.0.0.0 -d /tmp/_site
+jekyll serve --source /vagrant --force_polling --incremental --watch --host 0.0.0.0 -d /tmp/_site
